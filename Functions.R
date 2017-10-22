431be0fa35934952a327fff25d55dc811698c08f
Adam <12676475+adam-sampson@users.noreply.github.com>
2017-10-22 16:34:53 -0400

    Data Collection Complete

---
+++ Functions.R
@@ -0,0 +1,103 @@
+## Load libraries or ask to install them
+loadLibraries <- function(requiredPackages = NULL) {
+  for(pack in requiredPackages) {
+    # If statement will either load the library or give user the option to auto-install if not available
+    if(!require(pack,character.only = TRUE)) {
+      # ask the user for permission to install
+      if(menu(c('yes','no'),title=paste0('Package ',pack,' not found. Would you like to install?'))==1) {
+        install.packages(pack)
+        delay(1000)
+        require(pack)
+      }
+    }
+  }
+}
+
+## Load Jefferson County data for addresses and zoning and output single residence addresses
+createResidentialAddressList <- function(jcky.zoning, jcky.addresses) {
+  
+  if(is.character(jcky.zoning)==TRUE && file.exists(jcky.zoning)) {
+    jcky.zoning <- read_csv(jcky.zoning)
+  } else if(is.data.frame(jcky.zoning)==FALSE) {
+    stop("Expecting file name string or dataframe as input")
+  }
+  
+  if(is.character(jcky.addresses)==TRUE && file.exists(jcky.addresses)) {
+    jcky.addresses <- read_csv(jcky.addresses)
+  } else if(is.data.frame(jcky.addresses)==FALSE) {
+    stop("Expecting file name string or dataframe as input")
+  }
+    
+  
+  jcky.zoning <- jcky.zoning %>% select(ZONING_COD,ZONING_NAM,ZONING_TYP) %>%
+                 distinct()
+  names(jcky.zoning)[1] <- "ZONING_CODE"
+  
+  
+  jcky.addresses <- jcky.addresses %>% select(FULL_ADDRESS,APT,MUNI_NAME,ZIPCODE,ZONING_CODE)
+  jcky.addresses <- jcky.addresses %>% filter(is.na(APT)==TRUE) %>% select(-APT)
+  
+  jcky.addresses <- left_join(jcky.addresses,jcky.zoning)
+  jcky.addresses <- jcky.addresses %>% filter(ZONING_TYP == "RESIDENTIAL") %>%
+                    filter(ZONING_NAM == "RES SINGLE FAMILY" | ZONING_NAM == "RES SINGLE FAMILY OVERLAY" | 
+                           ZONING_NAM == "RURAL RESIDENTIAL" | ZONING_NAM == "RURAL RES OVERLAY")
+}
+
+recursiveListExtract <- function(xml.obj) {
+  ## We can take advantage of the fact that only objects without children have values
+  
+}
+
+flattenZillowList <- function(in.list) {
+  # flatten the list 
+  in.list <- unlist(in.list)
+  
+  # cleanup the output to usable format
+  # each actual name/value is in format name = in.list[n] value = in.list[n+2] so check for this
+  out.mat <- NULL
+  for(cnt in 1:(length(in.list)-2)) {
+    if(endsWith(attributes(in.list[cnt])$names,"name") == TRUE && 
+       endsWith(attributes(in.list[cnt+2])$names,"value") == TRUE) {
+      temp <- matrix(data=in.list[[cnt+2]],dimnames = list(NULL,in.list[[cnt]]))
+      out.mat <- cbind(out.mat,temp)
+    }
+  }
+  out.mat <- as.data.frame(out.mat,stringsAsFactors=FALSE)
+  return(out.mat)
+}
+
+multipleDeepSearchZillow <- function(in.df, posVector) {
+  zillowSearchOut <- NULL
+  for(i in posVector) {
+    zillowTemp <- GetDeepSearchResults(address = in.df$FULL_ADDRESS[i],citystatezip = as.character(in.df$ZIPCODE[i]),zws_id = get_zillow_web_service_id())
+    zillowTemp <- flattenZillowList(zillowTemp)
+    zillowSearchOut <- bind_rows(zillowSearchOut,zillowTemp)
+  }
+  return(zillowSearchOut)
+}
+
+multipleDeepCompsZillow <- function(zpidVector,count) {
+  zillowUpdatedOut <- NULL
+  for(i in 1:length(zpidVector)) {
+    zillowTemp <- GetDeepComps(zpid = as.character(zpidVector[i]),count=count,zws_id = get_zillow_web_service_id())
+    if(startsWith(zillowTemp[[2]]$text,"Error")==FALSE) {
+      for(j in 1:length(zillowTemp[[3]][[1]][[2]])) {
+        zillowTemp2 <- flattenZillowList(zillowTemp[[3]][[1]][[2]][j])
+        zillowUpdatedOut <- bind_rows(zillowUpdatedOut,zillowTemp2)
+      }
+    }
+  }
+  return(zillowUpdatedOut)
+}
+
+multipleUpdatedPropertyDetails <- function(zpidVector) {
+  zillowUpdatedOut <- NULL
+  for(i in 1:length(zpidVector)) {
+    zillowTemp <- GetUpdatedPropertyDetails(zpid = as.character(zpidVector[i]),zws_id = get_zillow_web_service_id())
+    if(startsWith(zillowTemp[[2]]$text,"Error")==FALSE) {
+      zillowTemp <- flattenZillowList(zillowTemp)
+      zillowUpdatedOut <- bind_rows(zillowUpdatedOut,zillowTemp)
+    }
+  }
+  return(zillowUpdatedOut)
+}
---
+++ ZillowAnalysis.R
@@ -0,0 +1,79 @@
+#####################
+#
+#####################
+
+#---
+# Install Necessary Resources
+#---
+  source('Functions.R')  
+  requiredPackages <- c('ZillowR',
+                        'readr',
+                        'tidyverse',
+                        'RSQLite',
+                        'DBI',
+                        'XML')
+  loadLibraries(requiredPackages)
+  rm(requiredPackages)
+
+  source('Functions.R')
+  source('zillowAPIcredentials.R')
+  # Remove source zillow credentials and insert your ZWSID below
+  # set_zillow_web_service_id(ZWSID)
+  #   rm(ZWSID)
+
+#---
+# Create a local database to store data
+#---
+  mydb <- dbConnect(RSQLite::SQLite(),"my-db.sqlite")
+  
+    
+#---
+# Create a list of addresses from Jefferson County Public Data
+#---
+  # http://data.lojic.org/
+  
+  jcky.addresses <- createResidentialAddressList('Jefferson_County_KY_Zoning.csv',
+                                                 'Jefferson_County_KY_Address_Points.csv')
+  # dbWriteTable(mydb, "jckyaddresses", jcky.addresses)  
+  
+  # Selecting a random list of 6k addresses. Given an API limit of 1k per day, this
+  # should cover the needs of an analysis due in 6 days.
+  jcky.tolookup <- jcky.addresses %>% sample_n(6000, replace=FALSE)
+  dbWriteTable(mydb, "jckytolookup", jcky.tolookup)
+  rm(jcky.addresses)
+
+#---
+# Create list of data for median house price per month
+#---
+  medianHousePrice.df <- read_csv("MSPNHSUS.csv")
+   dbWriteTable(mydb,"medianHousePrice",medianHousePrice.df)
+#---
+# Download Zillow data from API
+#---
+  # Get 500 addresses from list of addresses and do a deep search on zillow
+  zillowSearch.df <- multipleDeepSearchZillow(jcky.tolookup,1000:1500)
+    dbWriteTable(mydb, "zillowDeepSearch",zillowSearch.df)
+  
+  # Unfortunately, the deep search doesn't include enough information.
+  # Also, many of these randomly selected properties are not in the Updated data 
+  # because they have not been sold recently enought to be in Zillow database.
+  # However, most comps have been updated with full details, and comps have a bit
+  # more information themselves...so next step is to get comps, we should be able
+  # to get 25 per zpid and per API call...so let's do it.
+  zillowComps.df <- multipleDeepCompsZillow(zillowSearch.df$zpid,25)
+    # if total rooms is empty there is less likely to be Updated data...so remove those
+    zillowComps.df <- zillowComps.df %>% filter(is.na(totalRooms)==FALSE)
+    
+  
+  # It is possible these comps are duplicates. Need to fix that.
+  zillowComps.df <- zillowComps.df[duplicated(zillowComps.df$zpid)==FALSE,]
+    dbWriteTable(mydb, "zillowDeepComps",zillowComps.df)
+    
+  # Finally, what we really want is the Updated data because it is quite rich. API calls
+  # are limited so 500 of these should be a good number.
+  zillowUpdates <- multipleUpdatedPropertyDetails(zillowComps.df$zpid[1:500])
+    
+#---
+# Combine median house prices, zillow deep comps data and zillow update data, then clean results for analysis
+#---
+  
