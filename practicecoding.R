b8a6f8bf934911c5b6058f339902a353958f8b7b
Adam <12676475+adam-sampson@users.noreply.github.com>
2017-10-26 16:05:30 -0400

    close to recursive function

--- practicecoding.R
+++ practicecoding.R
@@ -31,24 +31,65 @@ zillowTemp$response
 testToDF1 <- data.frame(matrix(zillowTemp[[3]][[1]][[2]][[1]], nrow=1, byrow=T))
 testToDF2 <- data.frame(matrix(zillowTemp[[3]][[1]][[2]][[1]], nrow=1, byrow=T),stringsAsFactors=FALSE)
 
+# recursiveListExtract <- function(input.list,parentName = NULL) {
+#   # out <- data.frame()
+#   #print(attributes(input.list)$names)
+#   if(("children" %in% attributes(input.list)$names) & !("value" %in% attributes(input.list)$names)) {
+#     print("Debug - Detected children in list")
+#     #recursively call the function
+#     for(each.list in input.list$children){
+#       temp <- recursiveListExtract(each.list,parentName = each.list$name)
+#       if(exists("out")) {
+#         out <- cbind(out,temp)
+#       } else {
+#         out <- temp
+#       }
+#         
+#     }
+#   } 
+#   if(("value" %in% attributes(input.list)$names)) {
+#     print("Debug - No children detected in list")
+#     # There are no children so we must be at the end of the branch
+#     # for(i in 1:length(input.list)) {
+#       print("for value 0")
+#       print(parentName)
+#       print(input.list$value)
+#       tempdf <- data.frame(paste0(input.list$value))
+#       print("for value 1")
+#       names(tempdf) <- parentName
+#       print(tempdf)
+#       print("for value 2")
+#       # out <- cbind(out,tempdf)
+#       out <- tempdf
+#     # }
+#   }
+#   print("return")
+#   return(out)
+# }
+
 recursiveListExtract <- function(input.list) {
-  out <- data.frame()
-  if("children" %in% attributes(input.list)$names) {
+  if(("children" %in% attributes(input.list)$names) & !("value" %in% attributes(input.list$children$text)$names)) {
     print("Debug - Detected children in list")
     #recursively call the function
     for(each.list in input.list$children){
       temp <- recursiveListExtract(each.list)
-      out <- cbind(out,temp)
+      if(exists("out")) {
+        out <- cbind(out,temp)
+      } else {
+        out <- temp
+      }
     }
-  } else {
-    print("Debug - No children detected in list")
-    # There are no children so we must be at the end of the branch
-    for(i in 1:length(input.list)) {
-      tempdf <- data.frame(input.list[[i]])
-      names(tempdf) <- attributes(input.list[i])[[1]]
-      out <- cbind(out,tempdf)
-    }
+  } 
+  if(("value" %in% attributes(input.list$children$text)$names)) {
+    print("Values detected - convert") 
+    print(input.list$name)
+    print(input.list[[1]])
+    out <- data.frame(as.character(input.list[[1]]))
+    colnames(out) <- input.list$name
   }
+  # if(exists("out") == FALSE) {
+  #   out <- NULL
+  # }
   return(out)
 }
 
